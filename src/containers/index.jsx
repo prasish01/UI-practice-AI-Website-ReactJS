5f686ae715d21dbfbb7a11f27423478c53fb8722
prasish01 <prasish01@gmail.com>
2024-11-17 23:54:59 +0545

    commit

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -3,4 +3,4 @@ export { default as Features } from "./features/Features";
 export { default as Footer } from "./footer/Footer";
 export { default as Header } from "./header/Header";
 export { default as Possiblity } from "./possiblity/Possiblity";
-export { default as What } from "./whatGPT3/what";
+export { default as What } from "./whatGPT3/What";
